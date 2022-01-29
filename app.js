207acd11f45309d5493ebf2e53860ed27d953271
kguzmanus267 <77806848+kguzmanus267@users.noreply.github.com>
2022-01-28 21:38:25 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('Why is sand so optimistic? It has a can-dune attitude');
+let life = 100; // global
+
+const hit = 10; 
+
+if (life == 100) {
+    let life = 100;
+    console.log(life);
+}//block
+
+if (life != 100) {
+    let life = 0;
+    console.log(life);
+} //block
+
+life = life - hit;
+
+life = life - hit;
+
+var name = "life";
+
+console.log(life);
+
+
