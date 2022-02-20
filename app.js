06e81e8602e99e0c0c6bac60a80932404bf78437
kguzmanus267 <77806848+kguzmanus267@users.noreply.github.com>
2022-02-19 19:56:06 -0600

    2/11
    Very late, sorry, had family issues last week. slowly getting back on track.

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
-var functions = [loseRed, winGreen];
+
+document.addEventListener('keydown' , function (righty) {
+    switch (righty.keyCode) {
+       case 39:
+          console.log("Right key is pressed.");
+          break;
+          };
+
+ document.addEventListener('keydown' , function (lefty) {
+    switch (lefty.keyCode) {
+        case 37:
+           console.log("Left key is pressed.");
+           break;
+           };
+
+ var directionChoices = {righty, lefty};
+
+var randomize = Math.floor(Math.random()*directionChoices.length);
+
+  if (randomize) {winGreen();
+} else {loseRed();
+}
+var functions = {loseRed, winGreen};
 
 function loseRed () {
     document.body.style.backgroundColor ='red';
@@ -9,10 +31,7 @@ function winGreen(){
     document.body.style.backgroundColor = 'green';
     document.body.style.color = 'white';
     console.log("WIN");
-}
+    
 
-function randomize(n) {
-     return [Math.floor(Math.random()*n)];
-}
 
-functions[randomize(functions.length)]();
+}})});
